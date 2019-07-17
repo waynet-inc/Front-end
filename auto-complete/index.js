b9a68f291aed3f6a0eb3708a095a535c10a2c5b3
waynet-inc <valeriy.upw@gmail.com>
2019-07-17 19:26:53 +0500

    feat(auto-complete): add insensitive case

--- auto-complete/index.js
+++ auto-complete/index.js
@@ -1,15 +1,17 @@
-module.exports = function createAutoComplete(data) {
-	
-	return function(input){
-		
-		let output = [];
-		
-		for (let i = 0; i < data.length; i++) {
-			if (data[i].startsWith(input)) {
-				output.push(data[i]);
-			}
-		}
+module.exports = {
+  createAutoComplete: function(data) {
+	  
+	  const f = function solution(input){
+		  if (Array.isArray(input) && array.length) {
+			  let output = [];
+			  for (let i = 0; i < data.length; i++) {
+				  if (data[i].toLowerCase().startsWith(input.toLowerCase())) {
+					  output.push(data[i]);
+					  }
+				}
 		return output;
-	}
-	
-}
+		} else { return []; }
+	}	
+	return f;	  
+  }
+};
