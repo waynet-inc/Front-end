8e7c6c47201cbe23981910ebeccc9da7a0f2ed86
waynet-inc <valeriy.upw@gmail.com>
2019-07-26 17:53:00 +0500

    refactor(inheritance): remove index.js

--- inheritance/StringBuilder.js
+++ inheritance/StringBuilder.js
@@ -8,21 +8,6 @@ StringBuilder.prototype = Object.create(Builder.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
 StringBuilder.prototype.super = Builder.prototype;
 
-//StringBuilder.prototype.plus = function(...str) {
-//      this.value += str.join('');
-//      return this;
-//}
-
-//StringBuilder.prototype.minus = function(n) {
-//    this.value = this.value.slice(0, -n);
-//    return this;
-//}
-
-//StringBuilder.prototype.divide = function(n) {
-//   this.value = this.value.slice(0, Math.floor(this.value.length/n) + 1);
-//   return this;
-//}
-
 StringBuilder.prototype.remove = function(str) {
     let n = this.value.indexOf(str);
     const len = str.length;
