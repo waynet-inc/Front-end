e222dfe184c088cdd6dc6ae0a41070667b56aa2e
waynet-inc <valeriy.upw@gmail.com>
2019-08-23 01:40:44 +0500

    feat(custom-lodash.test): add tests for _.chunk

--- custom-lodash-tests/test/custom-lodash.test.js
+++ custom-lodash-tests/test/custom-lodash.test.js
@@ -1,11 +1,45 @@
 const _ = require('./../js/custom-lodash.js');
 
+describe("CustomLodash: ", () => {
 
-describe("CustomLodash", function () {
+    /** 
+     * 
+     * @function _.chunk(array, [size=1])
+     * @param {Array} array The array to process.
+     * @param {number} [size=1] The length of each chunk
+     * @returns {Array} Returns the new array of chunks.
+     * 
+     */
 
-    it("chunk should create an array of elements split into groups the length of size", () => {
-        expect(_.chunk(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]);
-        expect(_.chunk(['a', 'b', 'c', 'd'], 3)).toEqual([['a', 'b', 'c'], ['d']]);
-        expect(_.chunk(['a', 'b', 'c', 'd'], -2)).toEqual([]);
+    describe("_.chunk ", () => {
+        
+        it("should create an array of elements split into groups the length of size", () => {
+            expect(_.chunk(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]); 
+            expect(_.chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2)).toEqual([['a', 'b'], ['c', 'd'], ['e', 'f']]);
+        });
+
+        it(": if array can't be split evenly, the final chunk should contain the remaining elements", () => {
+            expect(_.chunk(['a', 'b', 'c', 'd'], 3)).toEqual([['a', 'b', 'c'], ['d']]);
+            expect(_.chunk(['a', 'b', 'c', 'd'], 5)).toEqual([['a', 'b', 'c', 'd']]); 
+        });
+
+        it(": if size is omitted, the size should be 1", () => {
+            expect(_.chunk(['a', 'b', 'c', 'd'])).toEqual([['a'], ['b'], ['c'], ['d']]);
+        });
+
+        it(": if size < 0, method should return an empty array", () => {
+            expect(_.chunk(['a', 'b', 'c', 'd'], -2)).toEqual([]);
+        });
+
+        it(": if first param is not an array, method should throw an error", () => {
+            expect(() => _.chunk(1024, 5)).toThrow();
+            expect(() => _.chunk({user: 'barney'}, 5)).toThrow();
+        });
+
+        // --> Infinity Loop
+        xit(": if size = 0, method should return an empty array", () => {
+            expect(_.chunk(['a', 'b', 'c', 'd'], 0)).toEqual([]);
+        });
     });
+    
 });
